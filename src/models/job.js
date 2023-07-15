4a99a7f51d441d55e36f1781cbd22f766d94fd25
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-07-15 13:23:07 +0400

    feat: connect to db

--- src/models/job.js
+++ src/models/job.js
@@ -1,11 +1,67 @@
-import mongoose from "mongoose";
+import { Schema, model } from "mongoose";
 
-const jobSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  image: String,
+const jobSchema = new Schema({
+  company: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  logo: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  logoBackground: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  position: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  postedAt: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  contract: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  location: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  website: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  apply: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  description: {
+    type: Schema.Types.String,
+    required: true,
+  },
+  requirements: {
+    content: {
+      type: Schema.Types.String,
+      required: true,
+    },
+    items: {
+      type: Schema.Types.Array,
+      required: true,
+    },
+  },
+  role: {
+    content: {
+      type: Schema.Types.String,
+      required: true,
+    },
+    items: {
+      type: Schema.Types.Array,
+      required: true,
+    },
+  },
 });
 
 const Job = mongoose.model("Job", jobSchema);
-
 export default Job;
