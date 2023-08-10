33d3c5e0c3fc469ef8dc2bb6bcc2dee45efd8b08
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-08-10 14:54:54 +0400

    feat: add swagger configuration

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import { router } from "./routes/jobRoutes.js";
 import { connection } from "./config/mongo.js";
 import dotenv from "dotenv";
-
+import swaggerMiddleware from "./middleware/swagger-middleware.js";
 
 const app = express();
 
@@ -13,6 +13,8 @@ connection();
   app.use("/img", express.static("public/images"));
   app.use("/jobs", router);
 
+
+  app.use("/", ...swaggerMiddleware());
   app.listen(3000, () => {
     console.log("Server is listening on port 3000");
   });
