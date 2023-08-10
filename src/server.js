e6283164819ff21ff51e035538f8570dfbb690ec
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-08-10 14:32:03 +0400

    feat: add path for images

--- src/server.js
+++ src/server.js
@@ -2,29 +2,18 @@ import express from "express";
 import { router } from "./routes/jobRoutes.js";
 import { connection } from "./mongo.js";
 import dotenv from "dotenv";
-import fs from "fs";
-import Job from "./models/job.js";
+
 
 const app = express();
 
 dotenv.config();
+connection();
 
-(async () => {
-  try {
-    await connection();
-    await Job.deleteMany({});
 
-    const data = JSON.parse(fs.readFileSync("data.json"));
-    await Job.insertMany(data);
-    console.log(data.length, "documents inserted.");
-  } catch (error) {
-    console.error("Error inserting data into MongoDB:", error);
-  }
-
-  app.use(express.json());
+  app.use("/img", express.static("public/images"));
   app.use("/jobs", router);
 
   app.listen(3000, () => {
     console.log("Server is listening on port 3000");
   });
-})();
+
